f36672e65db079009554059fdb04d023517c31da
huntspehre <hkrishna.hp@gmail.com>
2018-08-19 23:38:12 +0530

    Initial commit

---
+++ app/src/main/java/hunt/canedevelopment/LoginActivity.java
@@ -0,0 +1,20 @@
+package hunt.canedevelopment;
+
+import android.graphics.Typeface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        //TextView text_view = new TextView(this);
+       // Typeface font = Typeface.createFromAsset(getAssets(), "DV_ME_Shree0715.ttf");
+        //text_view.setTypeface(font);
+        //text_view.setText("मराठी");
+    }
+}
